afb094528945ca3d3d15797c4b1f76257f78808b
luizfernandoin <luizfernandoinfo100@gmail.com>
2024-05-28 10:09:08 -0300

    feat: versão final

--- config.py
+++ config.py
@@ -5,10 +5,16 @@ DEBUG = True
 
 SECRET_KEY = ENV("SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = "postgresql://lupismo_user:slhnTESKYbHWxPFFcFDq9f6MZXfOBvmF@dpg-cpasqvlds78s73d7jr7g-a.oregon-postgres.render.com/lupismo_41i3"
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'postgresql',
+        usuario = ENV("USUARIO"),
+        senha = ENV("SENHA"),
+        servidor = 'localhost',
+        database = ENV("DATABASE")
+    )
 
-#postgres://lupismo_user:slhnTESKYbHWxPFFcFDq9f6MZXfOBvmF@dpg-cpasqvlds78s73d7jr7g-a.oregon-postgres.render.com/lupismo_41i3
-
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 # pegando o nome do caminho absoluto do diretório e concatenando com o diretório uplouds
